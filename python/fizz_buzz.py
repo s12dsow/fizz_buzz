4d8703995e05e1f267b1df3c164a5b7653613ae1
Dicko S <s12.dsow@gmail.com>
2016-02-17 10:36:21 -0500

    completed fizzbuzz in python

---
+++ python/fizz_buzz.py
@@ -0,0 +1,8 @@
+def fizz_buzz(num):
+    string = ""
+    if num % 3 == 0:
+        string += "fizz"
+    if num % 5 == 0:
+        string += "buzz"
+
+    return string
---
+++ python/fizz_buzz_test.py
@@ -0,0 +1,34 @@
+import unittest
+from fizz_buzz import fizz_buzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+    """Tests for `fizzbuzz.py` ."""
+
+    def test_input_of_3_is(self):
+        """input of 3 returns fizz"""
+        self.assertEqual(fizz_buzz(3), 'fizz')
+
+    def test_multiple_of_3_is(self):
+        """multiple of 3 returns fizz"""
+        self.assertEqual(fizz_buzz(9), 'fizz')
+
+    def test_input_of_5_is(self):
+        """input of 5 returns buzz"""
+        self.assertEqual(fizz_buzz(5), 'buzz')
+
+    def test_multiple_of_5_is(self):
+        """multiple of 5 returns buzz"""
+        self.assertEqual(fizz_buzz(10), 'buzz')
+
+    def test_input_of_15_is(self):
+        """input of 15 returns fizzbuzz"""
+        self.assertEqual(fizz_buzz(15), 'fizzbuzz')
+
+    def multiple_of_15_is(self):
+        """multiple of 15 returns fizzbuzz"""
+        self.assertEqual(fizz_buzz(30), 'fizzbuzz')
+
+
+if __name__ == '__main__':
+    unittest.main()
