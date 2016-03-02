edb093413e4702e00adabbb6c81871d45d485c8f
Dicko S <s12.dsow@gmail.com>
2016-03-01 23:54:24 -0500

    reconfigured java project to use gradle

--- java/src/main/java/FizzBuzz.java
+++ java/src/main/java/FizzBuzz.java
@@ -1,5 +1,4 @@
-public class Main {
-
+public class FizzBuzz {
     public String fizzBuzz(Integer input) {
         String string = (input % 3 == 0) ? "fizz" : "";
         string += (input % 5 == 0) ? "buzz" : "";
--- java/src/test/java/FizzBuzzTest.java
+++ java/src/test/java/FizzBuzzTest.java
@@ -1,47 +1,46 @@
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class MainTest {
-
-    private Main main;
+public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
 
     @Before
     public void setUp() {
-        main = new Main();
+        fizzBuzz = new FizzBuzz();
     }
 
     @Test
     public void testFizzBuzzWhenInputIs3() {
-        String input = main.fizzBuzz(3);
+        String input = fizzBuzz.fizzBuzz(3);
 
         assertEquals("fizz", input);
     }
-
     @Test
     public void testFizzBuzzWhenInputIsMultipleOf3() {
-        String input = main.fizzBuzz(9);
+        String input = fizzBuzz.fizzBuzz(9);
 
         assertEquals("fizz", input);
     }
 
     @Test
     public void testFizzBuzzWhenInputIs5() {
-        String input = main.fizzBuzz(5);
+        String input = fizzBuzz.fizzBuzz(5);
 
         assertEquals("buzz", input);
     }
 
     @Test
     public void testFizzBuzzWhenInputIsMultipleOf5() {
-        String input = main.fizzBuzz(10);
+        String input = fizzBuzz.fizzBuzz(10);
 
         assertEquals("buzz", input);
     }
 
     @Test
     public void testFizzBuzzWhenInputIs15() {
-        String input = main.fizzBuzz(15);
+        String input = fizzBuzz.fizzBuzz(15);
 
         assertEquals("fizzbuzz", input);
     }
